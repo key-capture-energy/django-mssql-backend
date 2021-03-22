a189cc10bd1fcd6ea6d3937b19bbacda00a6dbf3
andrewbutlerboudakiankce <51485517+andrewbutlerboudakiankce@users.noreply.github.com>
2021-03-22 09:55:20 -0400

    Revert "Overrode and stripped all db_table references in base DB backend"
    This reverts commit e1c9b499f86de997640b720ab2f2e75232ef562d.

--- sql_server/pyodbc/introspection.py
+++ sql_server/pyodbc/introspection.py
@@ -47,63 +47,6 @@ class DatabaseIntrospection(BaseDatabaseIntrospection):
 
     ignored_tables = []
 
-    def django_table_names(self, only_existing=False, include_views=True):
-        """
-        Return a list of all table names that have associated Django models and
-        are in INSTALLED_APPS.
-        If only_existing is True, include only the tables in the database.
-        """
-        tables = set()
-        for model in self.get_migratable_models():
-            if not model._meta.managed:
-                continue
-            tables.add(model._meta.db_table.split('/')[0])
-            tables.update(
-                f.m2m_db_table() for f in model._meta.local_many_to_many
-                if f.remote_field.through._meta.managed
-            )
-        tables = list(tables)
-        if only_existing:
-            existing_tables = set(self.table_names(include_views=include_views))
-            tables = [
-                t
-                for t in tables
-                if self.identifier_converter(t) in existing_tables
-            ]
-        return tables
-
-    def installed_models(self, tables):
-        """
-        Return a set of all models represented by the provided list of table
-        names.
-        """
-        tables = set(map(self.identifier_converter, tables))
-        return {
-            m for m in self.get_migratable_models()
-            if self.identifier_converter(m._meta.db_table.split('/')[0]) in tables
-        }
-
-    def sequence_list(self):
-        """
-        Return a list of information about all DB sequences for all models in
-        all apps.
-        """
-        sequence_list = []
-        with self.connection.cursor() as cursor:
-            for model in self.get_migratable_models():
-                if not model._meta.managed:
-                    continue
-                if model._meta.swapped:
-                    continue
-                sequence_list.extend(self.get_sequences(cursor, model._meta.db_table.split('/')[0], model._meta.local_fields))
-                for f in model._meta.local_many_to_many:
-                    # If this is an m2m using an intermediate table,
-                    # we don't need to reset the sequence.
-                    if f.remote_field.through._meta.auto_created:
-                        sequence = self.get_sequences(cursor, f.m2m_db_table())
-                        sequence_list.extend(sequence or [{'table': f.m2m_db_table(), 'column': None}])
-        return sequence_list
-
     def get_field_type(self, data_type, description):
         field_type = super().get_field_type(data_type, description)
         # the max nvarchar length is described as 0 or 2**30-1
